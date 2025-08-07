74ee300ab8d041258ee3be98052bc3b30af92115
Gniewomir Świechowski <gniewomir.swiechowski@gmail.com>
2025-08-07 10:26:37 +0200

    database: chang initial migration, scaffold database entities

---
+++ src/infrastructure/database/entities/task.entity.ts
@@ -0,0 +1,55 @@
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  ManyToOne,
+  PrimaryColumn,
+  UpdateDateColumn,
+} from "typeorm";
+import { Goal } from "@infrastructure/database/entities/goal.entity";
+import { Context } from "@infrastructure/database/entities/context.entity";
+import { User } from "@infrastructure/database/entities/user.entity";
+
+@Entity()
+export class Task {
+  @PrimaryColumn("uuid")
+  id: string;
+
+  @Column({
+    nullable: false,
+    unique: true,
+    type: "bigint",
+  })
+  ordinalNumber: number;
+
+  @Column({
+    nullable: false,
+    unique: false,
+    type: "text",
+  })
+  description: string;
+
+  @CreateDateColumn()
+  createdAt: Date;
+
+  @UpdateDateColumn()
+  updatedAt: Date;
+
+  @Column()
+  goalId: string;
+
+  @ManyToOne(() => Goal, (goal) => goal.tasks, { lazy: true })
+  goal: Promise<Goal>;
+
+  @Column()
+  contextId: string;
+
+  @ManyToOne(() => Context, (context) => context.tasks, { lazy: true })
+  context: Promise<Context>;
+
+  @Column()
+  userId: string;
+
+  @ManyToOne(() => User, { lazy: true })
+  user: Promise<User>;
+}
