42af8d1ab5b9cdd427d17fa4305f64dfd950b0a7
Gniewomir Świechowski <gniewomir.swiechowski@gmail.com>
2025-08-14 21:00:24 +0200

    improv: make database columns naming consistent

--- src/infrastructure/database/entities/context.entity.ts
+++ src/infrastructure/database/entities/context.entity.ts
@@ -2,20 +2,19 @@ import {
   Column,
   CreateDateColumn,
   Entity,
+  ManyToOne,
   OneToMany,
   PrimaryColumn,
   UpdateDateColumn,
 } from "typeorm";
 
 import { Task } from "@infrastructure/database/entities/task.entity";
+import { User } from "@infrastructure/database/entities/user.entity";
 
 @Entity()
 export class Context {
   @PrimaryColumn("uuid")
   id: string;
-
-  @Column("uuid")
-  assignedId: string;
 
   @Column({
     nullable: false,
@@ -39,4 +38,10 @@ export class Context {
 
   @OneToMany(() => Task, (task) => task.goal, { lazy: true })
   tasks: Promise<Task[]>;
+
+  @Column("uuid")
+  userId: string;
+
+  @ManyToOne(() => User, { lazy: true })
+  user: Promise<User>;
 }
--- src/infrastructure/database/migrations/1755194186192-add-owner-to-contexts-and-goals.ts
+++ src/infrastructure/database/migrations/1755194186192-add-owner-to-contexts-and-goals.ts
@@ -7,15 +7,15 @@ export class AddOwnerToContextsAndGoals1755194186192
 
   public async up(queryRunner: QueryRunner): Promise<void> {
     await queryRunner.query(`ALTER TABLE "context"
-      ADD "assignedId" uuid NOT NULL`);
+      ADD "userId" uuid NOT NULL`);
     await queryRunner.query(`ALTER TABLE "goal"
-      ADD "assignedId" uuid NOT NULL`);
+      ADD "userId" uuid NOT NULL`);
   }
 
   public async down(queryRunner: QueryRunner): Promise<void> {
     await queryRunner.query(`ALTER TABLE "goal"
-      DROP COLUMN "assignedId"`);
+      DROP COLUMN "userId"`);
     await queryRunner.query(`ALTER TABLE "context"
-      DROP COLUMN "assignedId"`);
+      DROP COLUMN "userId"`);
   }
 }
