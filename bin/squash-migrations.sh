ba4eb406d750424edd6389c8f7f259e352127572
Gniewomir Świechowski <gniewomir.swiechowski@gmail.com>
2025-08-16 21:27:42 +0200

    feat: script for squaring migrations - until possible

---
+++ bin/squash-migrations.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+set -e  # Exit on any error
+
+PROJECT_ROOT="$(cd "$(dirname "${BASH_SOURCE[0]}")/.." && pwd)"
+
+execute() {
+  if [ $# -eq 0 ]; then
+    echo "❌ Error: No command to execute."
+    return 1
+  fi
+
+  echo "▶️  Executing: '$*'..."
+
+  "$@" > /dev/null 2>&1
+
+  local exit_code=$?
+
+  if [ $exit_code -eq 0 ]; then
+    echo "✅ '$*' success!"
+  else
+    echo "❌ Error, returned exit code: $exit_code"
+  fi
+
+  return $exit_code
+}
+
+execute cd "$PROJECT_ROOT"
+execute find src/infrastructure/database/migrations -name "*.ts" -delete
+execute ./bin/reset-test-db.sh
+execute npm run migration:generate --name=initial
+execute npm run format
+execute npm run lint
+execute npm run migration:run
+execute git add src/infrastructure/database/migrations
