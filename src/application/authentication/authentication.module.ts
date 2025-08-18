3f76bd4014a0763422c2dfc6456560abf06cfca8
Gniewomir Świechowski <gniewomir.swiechowski@gmail.com>
2025-08-18 23:38:29 +0200

    feat: authentication service

--- src/application/authentication/authentication.module.ts
+++ src/application/authentication/authentication.module.ts
@@ -1,8 +1,12 @@
 import { Module } from "@nestjs/common";
 
 import { AuthenticationService } from "@application/authentication/authentication.service";
+import { ClockModule } from "@infrastructure/clock";
+import { ConfigModule } from "@infrastructure/config";
+import { JwtModule } from "@infrastructure/security/jwt";
 
 @Module({
+  imports: [ClockModule, ConfigModule, JwtModule],
   providers: [AuthenticationService],
   exports: [AuthenticationService],
 })
--- src/application/authentication/authentication.service.ts
+++ src/application/authentication/authentication.service.ts
@@ -1,4 +1,30 @@
-import { Injectable } from "@nestjs/common";
+import { Inject, Injectable } from "@nestjs/common";
+
+import { AuthenticationFacade } from "@domain/authentication/Authentication.facade";
+import { TokenPayload } from "@domain/authentication/OAuth/Token/TokenPayload";
+import {
+  TokenPayloadInterfaceSymbol,
+  TokenPayloadsInterface,
+} from "@domain/authentication/OAuth/Token/TokenPayloads.interface";
+import { ClockInterface, ClockInterfaceSymbol } from "@domain/Clock.interface";
+import { AuthConfig } from "@infrastructure/config/configs";
 
 @Injectable()
-export class AuthenticationService {}
+export class AuthenticationService {
+  constructor(
+    private readonly authConfig: AuthConfig,
+    @Inject(ClockInterfaceSymbol)
+    private readonly clock: ClockInterface,
+    @Inject(TokenPayloadInterfaceSymbol)
+    private readonly tokenPayloads: TokenPayloadsInterface,
+  ) {}
+
+  async authenticate(token: string): Promise<TokenPayload> {
+    return AuthenticationFacade.authenticate(
+      token,
+      this.tokenPayloads,
+      this.clock,
+      this.authConfig,
+    );
+  }
+}
